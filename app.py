b5e29af2d59a751c8e6fcccef03a894731ecdbaa
Abhijeet Shashwat <asp616848@gmail.com>
2025-01-23 14:55:42 +0530

    API for data retreival

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+# Load the data from data.json
+with open('data.json', 'r') as file:
+    student_data = json.load(file)
+
+@app.route('/api', methods=['GET'])
+def get_marks():
+    names = request.args.getlist('name')
+    result = {}
+    for name in names:
+        if name in student_data:
+            result[name] = student_data[name]
+        else:
+            result[name] = "Not found"
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
